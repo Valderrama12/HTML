325502ae102d5373f62a7c4a0b29380fb8b6256d
SERGIO <110955526+Valderrama12@users.noreply.github.com>
2022-10-12 10:00:24 -0500

    Add files via upload

---
+++ SEMANA 12/Quok Arrays.js
@@ -0,0 +1,4 @@
+        //Programa que permita crear un array – vector con los nombres de la semana, luego permita consultar y mostrar por pantalla cualquier día
+        var dias_semana=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"]
+        //Salida
+        console.log(dias_semana[8]);
